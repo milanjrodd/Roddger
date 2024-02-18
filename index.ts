5205201338651ec3dc97368728678dc846f61147
milanjrodd <milanrodd@gmail.com>
2024-02-18 20:31:08 +0300

    remove socket server logs

--- index.ts
+++ index.ts
@@ -8,18 +8,9 @@ Bun.serve({
     return router(req);
   },
   websocket: {
-    message(ws, message) {
-      console.log("Message from client: ", message);
-      ws.send("Hello from server!");
-    }, // a message is received
-    open(ws) {
-      ws.send("Hello from server!");
-    }, // a socket is opened
-    close(ws, code, message) {
-      console.log("Socket closed: ", code, message);
-    }, // a socket is closed
-    drain(ws) {
-      console.log("Socket is ready to receive more data");
-    }, // the socket is ready to receive more data
+    message(ws, message) {}, // a message is received
+    open(ws) {}, // a socket is opened
+    close(ws, code, message) {}, // a socket is closed
+    drain(ws) {}, // the socket is ready to receive more data
   },
 });
