d0ee3fe79f97a60f3e898369336f708300bf1a6e
milanjrodd <milanrodd@gmail.com>
2024-02-20 09:26:48 +0300

    update db migration strategy and enable wal mode

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,8 +1,15 @@
 import { drizzle } from "drizzle-orm/bun-sqlite";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 import { Database } from "bun:sqlite";
 import path from "path";
 
+// Create the database
 const databasePath = path.resolve(import.meta.dir, "db.sqlite");
-
 const sqlite = new Database(databasePath);
+sqlite.exec("PRAGMA journal_mode = WAL;"); // Enable WAL mode
 export const db = drizzle(sqlite);
+
+// Migrate the database
+console.log("Migrating database...");
+const migrationsFolderPath = path.resolve(import.meta.dir, "./migrations");
+await migrate(db, { migrationsFolder: migrationsFolderPath });
